c2ea20364f7577c4e1186a28e341a82dc24d1e70
GovindSharmaTW <govind@thoughtwin.com>
2024-03-08 19:16:22 +0530

    firebase and firebase authentication setup

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,14 +1,62 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const LoginScreen = (props) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    console.log("onAuthStateChanged function called", user);
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    console.log("Please Login");
+  }
+
+  const handleLogin = ({ email, password }) => {
     // Add your login logic here
-   props.navigation.navigate("Home");
-  };
+     props.navigation.navigate("Home");
+
+    // console.log("TT01 handle Login function called", email, password);
+    
+    // if ((email !== '' && email !== undefined) && ( password !== '' && password !== undefined )) {
+    //   auth()
+    //     .createUserWithEmailAndPassword(email, password)
+    //     .then(() => {
+    //       console.log('User account created & signed in!');
+    //     })
+    //     .catch(error => {
+    //       if (error.code === 'auth/email-already-in-use') {
+    //         console.log('That email address is already in use!');
+    //       }
+
+    //       if (error.code === 'auth/invalid-email') {
+    //         console.log('That email address is invalid!');
+    //       }
+
+    //       console.error(error);
+    //     });
+    // }
+    // else
+    // {
+    //   Alert.alert("Please write email and password");
+    // }
+  }
+
 
   return (
     <View style={styles.container}>
@@ -32,7 +80,7 @@ const LoginScreen = (props) => {
           onChangeText={(text) => setPassword(text)}
         />
       </View>
-      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+      <TouchableOpacity style={styles.loginBtn} onPress={() => handleLogin(email, password)}>
         <Text style={styles.loginText}>LOGIN</Text>
       </TouchableOpacity>
     </View>
@@ -55,8 +103,8 @@ const styles = StyleSheet.create({
   inputView: {
     width: '90%',
     backgroundColor: '#fff',
-    borderColor:'#203060',
-    borderWidth:1,
+    borderColor: '#203060',
+    borderWidth: 1,
     borderRadius: 25,
     height: 50,
     marginBottom: 20,
